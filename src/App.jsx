f2b7a831fafb62d195c9da013c8c95e88a33336e
Ax-cd <axcd.coder@gmail.com>
2022-03-30 16:24:34 +0200

    success render marked text in previewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { marked } from "marked";
+// import DOMPurify from "dompurify";
 
 // use backquote over quote => backquote allow linebreaks
-const defaultText = `#This is a markdown previewer
-##Write in the editor and preview the result
+const defaultText = `# This is a markdown previewer
+## Write in the editor and preview the result
 This is a [link](https://reactjs.org/docs/getting-started.html) to React Documentation.
 You can add inline code, like so:
 Or a code block
@@ -11,11 +12,17 @@ Do you need to list items ? You can:
 What about a > blockquote ?
 Emphasis the **importance** of an element with **bolded text**.
 And add images!
-![Img name](url)
+![Img name](https://picsum.photos/id/1050/200/300)
   `;
 
-// marked.setOptions({
+// marked.use({
+//   pedantic: false,
+//   gfm: true,
 //   breaks: true,
+//   sanitize: false,
+//   smartLists: true,
+//   smartypants: false,
+//   xhtml: false,
 // });
 
 export default class App extends Component {
@@ -29,9 +36,10 @@ export default class App extends Component {
     this.setState({ editorInput: event.target.value });
   };
 
-  getMarkdownText = () => {
-    let text = marked.parse(this.state);
-    return { __html: text };
+  getMarkdownText = (editorInput) => {
+    let __html = marked(editorInput);
+    // let cleanText = DOMPurify.sanitize(__html);
+    return { __html };
   };
 
   render() {
@@ -50,16 +58,15 @@ export default class App extends Component {
           ></textarea>
         </div>
 
-        <div id="preview">
+        <div>
           <label for="preview">Previewer</label>
           <br />
-          <input
-            type="text"
-            // value={this.state.editorInput}
-            // onChange={this.handleEditorChange}
-            dangerouslySetInnerHTML={this.getMarkdownText}
-            // dangerouslySetInnerHTML={{ __html: inputText }}
-          ></input>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={this.getMarkdownText(
+              this.state.editorInput
+            )}
+          />
         </div>
       </React.Fragment>
     );
